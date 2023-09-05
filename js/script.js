335866cced291c8639185d643cbd2a5153af4707
fabio <fabio@gmail.com>
2023-09-05 20:15:04 -0300

    primer parte terminada

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
+const tasks = ["Limpiar la pieza", "Hacer las compras", "Lavar la ropa"];
+
 const taskInput = document.querySelector("#taskInput");
 const addButton = document.querySelector("#addButton");
 const taskList = document.querySelector("#taskList");
 
+const showTasks = () => {
+  taskList.innerHTML = ""; //limpiamos la lista de tareas
+  tasks.forEach((task, index) => {
+    // cargamos la lista de tareas con el array
+    const li = document.createElement("li");
+    li.innerHTML = `
+      <span>${task}</span>
+      <button onclick="removeTask(${index})">❌</button>`;
+    taskList.appendChild(li);
+  });
+};
+
+const removeTask = (index) => {
+  //eliminamos una tarea del arreglo y actualizamos la vista
+  tasks.splice(index, 1); //elimina un elemento en especifico
+  showTasks();
+};
+
 const addTask = () => {
-  if (taskInput.value.trim() == "") return alert("Tarea vacia");
-  console.log("Tarea valida");
+  taskInput.value = taskInput.value.trim();
+  if (taskInput.value == "") return alert("Tarea vacia");
+  tasks.push(taskInput.value);
+  taskInput.value = "";
+  showTasks();
 };
 
 addButton.addEventListener("click", addTask);
+showTasks();
